bf3a7a18b70b04b78a4ea31cc6799e5aec08e973
Sanjana001 <sanjana0056.cse19@chitkara.edu.in>
2020-12-13 12:44:58 +0530

    py file

--- Image_Cartoonizer/image.py
+++ Image_Cartoonizer/image.py
@@ -4,11 +4,14 @@ import cv2
 img = cv2.imread(r"C:\Users\HP\Pictures\camera pictures\DSC01029.JPG")
 #resize the image
 #resize() function takes two arguments first is the source image and the second one is the dimension that you want to give in order to resize your image
+#detecting the edges of an image
 image = cv2.resize(img,(800,800))
 gray = cv2.cvtColor(image,cv2.COLOR_RGB2GRAY)
 gray = cv2.medianBlur(gray,5)
 edges = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,9,9)
+#colors
 colors = cv2.bilateralFilter(image,9,300,300)
+#cartoonize
 cartoon = cv2.bitwise_and(colors,colors,mask=edges)
 #imshow() function is used to display the image
 #It takes two arguments: 1.) name that you want to give to your displayed image
